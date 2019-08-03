e8bf55dbc3cb52b75e1578fb54be96b49726fd04
RafaelZapata <47725515+RafaelZapata@users.noreply.github.com>
2019-08-03 11:28:38 -0300

    Aula 10 - Functions

--- Aula 10/Ex2/script.js
+++ Aula 10/Ex2/script.js
@@ -5,8 +5,13 @@ btnElement.onclick = function validarEmail(){
     let email = inputElement.value;
     
     let divArroba = email.split('@');
-    let divPonto = email[1].split('.');
+    let divPonto = divArroba[1].split('.');
 
-    console.log(divArroba);
     console.log(divPonto);
+
+    if(divArroba[0] < 1 || divArroba.length != 2 || divPonto[0] < 1 || divPonto.length != 2) {
+        alert('Email invalido');
+    } else {
+        alert('Email valido');
+    }
 }
